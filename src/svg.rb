8a299166dc61f29e91a737d0d4d93d7dd82ec0b8
Florent Monnier <monnier.florent@gmail.com>
2024-08-18 22:41:32 +0200

    add circles

--- src/svg.rb
+++ src/svg.rb
@@ -11,20 +11,73 @@ class Svg
     xmlns=\"http://www.w3.org/2000/svg\"
     width=\"#{@width}#{@unit}\"
     height=\"#{@height}#{@unit}\"
-    viewBox=\"0 0 #{@width} #{@height}\">
+    viewBox=\"0 0 #{@width} #{@height}\">\n
   SVG
+    @is_finished = false
   end
 
   def finish_svg
     @buffer << "\n</svg>\n"
+    @is_finished = true
   end
 
-  def write
-    puts @buffer
+  def print
+    if @is_finished
+      puts @buffer
+    else
+      raise StandardError, "document not finished"
+    end
   end
+
+  def write(filename)
+    if @is_finished
+      File.open(filename, 'w') do |f|
+        f.write(@buffer)
+      end
+    else
+      raise StandardError, "document not finished"
+    end
+  end
+
+  def add_circle(cx, cy, r,
+        fill: "", fill_opacity: "",
+        stroke: "", stroke_width: "", stroke_opacity: "",
+        id: "", css: "", style: "")
+
+    if @is_finished
+      raise StandardError, "document already finished"
+    end
+    if fill != ""
+      fill = " fill=\"#{fill}\""
+    end
+    if fill_opacity != ""
+      fill_opacity = " fill-opacity=\"#{fill_opacity}\""
+    end
+    if stroke != ""
+      stroke = " stroke=\"#{stroke}\""
+    end
+    if stroke_width != ""
+      stroke_width = " stroke-width=\"#{stroke_width}\""
+    end
+    if stroke_opacity != ""
+      stroke_opacity = " stroke-opacity=\"#{stroke_opacity}\""
+    end
+    if style != ""
+      style = " style=\"#{style}\""
+    end
+    if css != ""
+      css = " css=\"#{css}\""
+    end
+    if id != ""
+      id = " id=\"#{id}\""
+    end
+
+    str = "<circle cx=\"#{cx}\" cy=\"#{cy}\" r=\"#{r}\""
+    str += id + css + style
+    str += fill + fill_opacity + stroke + stroke_width + stroke_opacity
+
+    @buffer << str + " />\n"
+  end
+
 end
-
-mon_doc = Svg.new(240, 180)
-mon_doc.finish_svg
-mon_doc.write
 
